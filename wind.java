e172379871b913f2c1d2ec17d4b0935ef6a65661
Aidan Forrester <agregforrester@gmail.com>
2020-09-29 13:10:25 -0400

    exercise 1

---
+++ wind.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.Math;
+
+        public class wind {
+            public static void main(String[] args) {
+                Scanner in = new Scanner(System.in);
+
+                System.out.println("Temperature : ");
+                double temperature = in.nextDouble();
+                in.nextLine();
+
+                System.out.println("Wind Speed : ");
+                double windSpeed = in.nextDouble();
+
+
+                double windChill = 35.74 + (0.6215 * temperature) + (((0.4275 * temperature) - 35.75) * (Math.pow(windSpeed, 0.16));
+
+                System.out.println(" ");
+                System.out.printf("$%.2f", windChill);
+                in.close();
+            }
+        }
