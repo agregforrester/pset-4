de9586d5dc48548ff095343ad1a07e4dca4218a3
Aidan Forrester <agregforrester@gmail.com>
2020-10-02 13:16:07 -0400

    exercise 2

---
+++ cornhole.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+        public class cornhole {
+            public static void main(String[] args) {
+                Scanner in = new Scanner(System.in);
+
+                System.out.println("Length   : ");
+                double length = in.nextDouble();
+                in.nextLine();
+
+                System.out.println("Width    : ");
+                double width = in.nextDouble();
+                in.nextLine();
+
+                System.out.println("Diameter : ");
+                double diameter = in.nextDouble();
+
+
+                double circleArea = (diameter / 2) * (diameter / 2) * 3.1415;
+                double boardArea = length * width;
+
+                double cornholeArea = boardArea - circleArea;
+
+                System.out.println(" ");
+                System.out.printf("Surface area : %,.2f", cornholeArea);
+                in.close();
+            }
+        }
