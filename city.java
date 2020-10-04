c32246f30ef8e6a1f0c5b9dcce5e9675564e455f
Aidan Forrester <agregforrester@gmail.com>
2020-10-04 13:51:17 -0400

    exercise 10

---
+++ city.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+        public class city {
+            public static void main(String[] args) {
+                Scanner in = new Scanner(System.in);
+
+                System.out.println("What's your favorite city? ");
+                String city = in.nextLine();
+
+                int length = city.length();
+
+                String upper = city.toUpperCase();
+
+                String lower = city.toLowerCase();
+
+                System.out.println(" ");
+                System.out.println("Text   : " + city);
+                System.out.println("Length : " + length);
+                System.out.println("Upper  : " + upper);
+                System.out.println("Lower  : " + lower);
+                in.close();
+            }
+        }
