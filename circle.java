9372fea694223a131b5d9c7629175a00a24e6371
Aidan Forrester <agregforrester@gmail.com>
2020-10-04 13:13:32 -0400

    exercise 5

---
+++ circle.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+        public class circle {
+            public static void main(String[] args) {
+                Scanner in = new Scanner(System.in);
+
+                System.out.println("Diameter : ");
+                double diameter = in.nextDouble();
+
+                double radius = diameter / 2;
+                double area = radius * radius * 3.1415;
+
+                double circumference = diameter * 3.1415;
+
+                System.out.println(" ");
+                System.out.printf("Area          : %,.2f", area);
+                System.out.println(" ");
+                System.out.printf("Circumference : %,.2f", circumference);
+                in.close();
+            }
+        }
