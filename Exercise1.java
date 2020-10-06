495045e1182c2994eded31e0f932e7e0b88c3c24
agregforrester <71345781+agregforrester@users.noreply.github.com>
2020-10-06 11:00:29 -0400

    Update Exercise1.java

--- Exercise1.java
+++ Exercise1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-        public class wind {
+        public class Exercise1 {
             public static void main(String[] args) {
                 Scanner in = new Scanner(System.in);
 
