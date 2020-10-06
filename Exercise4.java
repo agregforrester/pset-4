6ebfafbbb943e976b2a2a16547d12b59225cddcc
agregforrester <71345781+agregforrester@users.noreply.github.com>
2020-10-06 11:01:23 -0400

    Update Exercise4.java

--- Exercise4.java
+++ Exercise4.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-        public class conversion {
+        public class Exercise5 {
             public static void main(String[] args) {
                 Scanner in = new Scanner(System.in);
 
